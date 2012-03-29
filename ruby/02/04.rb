f236e043565f6e288e43747b40f1879e0cdb5b33
Roman Simecek <roman.simecek@cyt.ch>
2012-03-29 18:04:38 +0200

    Exercise 4 finished.

--- ruby/02/04.rb
+++ ruby/02/04.rb
@@ -6,3 +6,15 @@ puts "Excercise 4"
 # having any occurrences of a phrase anywhere in that line.
 # You will need to do a simple regular expression match and read lines from a file.
 # (This is surprisingly simple in Ruby.) If you want, include line numbers.
+
+def grep(searched = '', file_name = 'some_text.txt')
+  File.open(File.dirname(__FILE__).concat("/").concat(file_name)) do |file|
+    file.each_line { |line| search_line(searched, line)  }
+  end
+end
+
+def search_line(value, line)
+  puts line if line.include?value
+end
+
+grep('nonumy')
